694671fc67813241988c7f19a24994a65894d005
CraigSmith789 <craig789@gmail.com>
2021-02-09 17:14:53 -0500

    commit tree 0864e17e49e810b7b9856d780dbd18448932b142

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(ary, blk){
+  let a = []
+  for(let i = 0; i < ary.length; i++){
+    let r = blk(ary[i])
+    a.push(r)
+
+  }
+  return a
+}
+function reduce(ary, blk, stp ){
+  let i = 1;
+  let s = ary[0];
+  if (!!stp){
+    s = stp ;
+    i = 0;
+  
+  }     
+  
+  for(; i < ary.length; i++){
+    s = blk(ary[i],s);              
+  }
+  return s;
+}
